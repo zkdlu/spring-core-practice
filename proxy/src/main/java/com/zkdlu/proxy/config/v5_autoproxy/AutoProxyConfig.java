89fbaec57463646af7d8182aceb7a73a053452ee
zkdlu <zkdlu159951@gmail.com>
2022-07-04 20:01:13 +0900

    aspectj 표현식

--- proxy/src/main/java/com/zkdlu/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ proxy/src/main/java/com/zkdlu/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import com.zkdlu.proxy.config.AppV2Config;
 import com.zkdlu.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import com.zkdlu.proxy.trace.logtrace.LogTrace;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +16,30 @@ import org.springframework.context.annotation.Import;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-    @Bean
+//    @Bean
     public Advisor advisor1(LogTrace trace) {
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*", "order*", "save*");
+
+        LogTraceAdvice advice = new LogTraceAdvice(trace);
+
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+//    @Bean
+    public Advisor advisor2(LogTrace trace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* com.zkdlu.proxy.app..*(..))");
+
+        LogTraceAdvice advice = new LogTraceAdvice(trace);
+
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    @Bean
+    public Advisor advisor3(LogTrace trace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* com.zkdlu.proxy.app..*(..)) && !execution(* com.zkdlu.proxy.app..noLog(..))");
 
         LogTraceAdvice advice = new LogTraceAdvice(trace);
 
