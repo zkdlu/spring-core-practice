f8740fa8fffeedb1567373a5a3baac64c2e0e9d2
zkdlu <zkdlu159951@gmail.com>
2022-07-04 19:54:42 +0900

    auto proxy

--- proxy/src/main/java/com/zkdlu/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ proxy/src/main/java/com/zkdlu/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -1,29 +1,22 @@
-package com.zkdlu.proxy.config.v4_postprocessor;
+package com.zkdlu.proxy.config.v5_autoproxy;
 
 import com.zkdlu.proxy.config.AppV1Config;
 import com.zkdlu.proxy.config.AppV2Config;
 import com.zkdlu.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
-import com.zkdlu.proxy.config.v4_postprocessor.postprocessor.PackageLogTracePostProcessor;
 import com.zkdlu.proxy.trace.logtrace.LogTrace;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@Slf4j
-//@Configuration
+@Configuration
 @Import({AppV1Config.class, AppV2Config.class})
-public class BeanPostProcessorConfig {
+public class AutoProxyConfig {
 
     @Bean
-    public PackageLogTracePostProcessor logTracePostProcessor(LogTrace trace) {
-        return new PackageLogTracePostProcessor("com.zkdlu.proxy.app", getAdvisor(trace));
-
-    }
-
-    private Advisor getAdvisor(LogTrace trace) {
+    public Advisor advisor1(LogTrace trace) {
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*", "order*", "save*");
 
@@ -31,5 +24,4 @@ public class BeanPostProcessorConfig {
 
         return new DefaultPointcutAdvisor(pointcut, advice);
     }
-
 }
